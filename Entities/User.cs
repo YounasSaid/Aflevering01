0e10e086eabd93c57def8a618bef5642b1b1b12f
YounasSaid <336158@viauc.dk>
2025-09-11 18:28:03 +0200

    s

--- Entities/User.cs
+++ Entities/User.cs
@@ -2,10 +2,17 @@
 
 public class User
 {
-    
     public int Id { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
-        
     
+    // Parameterless constructor for EFC (kommer senere)
+    private User() { }
+    
+    // Constructor for creating new users
+    public User(string userName, string password)
+    {
+        UserName = userName;
+        Password = password;
+    }
 }
