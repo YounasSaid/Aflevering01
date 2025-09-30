8a3fafdfb0f9d3b2e7a156094455330e03d33d28
YounasSaid <336158@viauc.dk>
2025-09-30 13:59:49 +0200

    WebAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,18 @@
+using FileRepositories;
+using RepositoryContracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Register repositories for dependency injection
+builder.Services.AddScoped<IUserRepository, UserFileRepository>();
+builder.Services.AddScoped<IPostRepository, PostFileRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentFileRepository>();
 
 var app = builder.Build();
 
